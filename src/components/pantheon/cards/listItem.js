000c5a43ff58ff09e4155f4b6a269c15d6002275
thejhubbs <thejhubbs@gmail.com>
2020-02-20 16:05:47 -0500

    Big redesign & stuff

--- src/components/pantheon/cards/listItem.js
+++ src/components/pantheon/cards/listItem.js
@@ -6,21 +6,26 @@ import { Row, Col } from 'reactstrap'
 function SmallPantheonCard(props) {
       const item = props.pantheon
 
-      return <Link to={`/pantheons/${item.pantheon_id}`} className='blockLink'><Row>
-            <Col lg={4} xs={12}>
-                  <h2>{item.thumbnail ? <img src={item.thumbnail.image_url ? item.thumbnail.image_url : require('../../../img/logo.png')} height="50px" alt={item.pantheon_name} /> : item.pantheon_name[0]}</h2>
-            </Col>
-            <Col lg={4} xs={12}>
-                  <h3>{item.pantheon_name}</h3>
-            </Col>
-            <Col lg={4} xs={12}>
-                  <span style={{ padding: '5px' }}>{item.start_year < 0 ? item.start_year * -1 + "BC" : item.start_year + "AD"} - {item.end_year < 0 ? item.end_year * -1 + "BC" : (item.end_year === 2100 ? "Present" : item.end_year + "AD") }</span>
-            </Col>
-            <Col xs={12}>
-                  <br />
-                  <span style={{ padding: '10px' }}>{item.pantheon_description}</span>
-            </Col>
-      </Row></Link>
+      const image_thumb = (link) => {
+            const linkArr = link.split('/')
+            linkArr.splice(linkArr.length - 2, 1, 'w_800')
+            return linkArr.join('/')
+      }
+
+      return <Link to={`/pantheons/${item.pantheon_id}`} style={{ backgroundImage: `url(${item.image_url ? image_thumb(item.image_url) : null})` }} className='blockLink pantheonStyleLink'>
+            <Row style={{ width: '100%', height: '100%', marginLeft: '0' }}>
+                  <Col lg={3} xs={12} style={{padding:'0'}}>
+                        <h3 className="pantheonStyleName">{item.pantheon_name}</h3>
+                  </Col>
+                  <Col lg={9} xs={12} style={{padding:'0'}}>
+                        <p className="pantheonStyleText">
+                              {item.start_year < 0 ? item.start_year * -1 + "BC" : item.start_year + "AD"} -
+                        {item.end_year < 0 ? item.end_year * -1 + "BC" : (item.end_year === 2100 ? "Present" : item.end_year + "AD")}<br />
+                              {item.pantheon_description}
+                        </p>
+                  </Col>
+            </Row>
+      </Link>
 }
 
 export default SmallPantheonCard
--- src/components/symbol/lists/simple.js
+++ src/components/symbol/lists/simple.js
@@ -1,25 +1,53 @@
 import React from 'react'
 import SmallSymbolCard from '../cards/small'
-import {CSSTransition, SwitchTransition} from 'react-transition-group'
+import { CSSTransition, SwitchTransition } from 'react-transition-group'
 
 function SimpleSymbolList(props) {
     const style = {
         display: "flex",
         flexWrap: 'wrap',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        position: 'relative',
+        marginBottom:'50px'
+    }
+
+    const breadStyle = { 
+        position: 'absolute', 
+        left: '0', 
+        background: 'black',
+        marginLeft: '-48px',
+        width: '48px'
     }
 
 
     //a key is created by appending each id onto each other, creating at most a length-60 string, up to 100,000 entries
-    const renderKey = props.symbols.length > 1 ? props.symbols.reduce((sum, item) => { return `${sum}${item.symbol_id}`}) : "0"
+    const renderKey = props.symbols.length > 1 ? props.symbols.reduce((sum, item) => { return `${sum}${item.symbol_id}` }) : "0"
+
+    let lastLetter = ''
 
     return <SwitchTransition><CSSTransition key={renderKey}
-      in={true} timeout={150} classNames="search-page" unmountOnExit>
-      <div style={style} key={renderKey}>
-        { props.symbols.length > 0 ? props.symbols.map(item =>
-            <SmallSymbolCard symbol={item} key={item.symbol_name}  />
-        ) : "There are no results." }
-    </div></CSSTransition></SwitchTransition>
+        in={true} timeout={150} classNames="search-page" unmountOnExit>
+        <div style={style} key={renderKey}>
+            {props.symbols.length > 0 ? props.symbols.map(item =>
+
+                <>
+                <div style ={{
+    }}>
+                    {
+                        //Random check to see if the length is greater than 50 in order to display "bread crumbs"
+                        lastLetter != item.symbol_name[0] && props.symbols.length > 50 ?
+                            <div style={breadStyle}>
+                                <h3>{lastLetter = item.symbol_name[0]} 
+                                    &nbsp;<span style={{color:'#558'}} className='fas fa-long-arrow-alt-down'></span></h3>
+                            </div>
+                            : null
+                    }
+                    </div>
+                    <SmallSymbolCard symbol={item} key={item.symbol_name} />
+                </>
+
+            ) : "There are no results."}
+        </div></CSSTransition></SwitchTransition>
 }
 
 export default SimpleSymbolList
