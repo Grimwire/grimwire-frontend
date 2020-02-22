c5c8b1aedde3b9d256fc161b61544e426d2e3fee
thejhubbs <thejhubbs@gmail.com>
2020-02-22 15:51:49 -0500

    small p

--- src/components/pantheon/cards/listItem.js
+++ src/components/pantheon/cards/listItem.js
@@ -15,13 +15,14 @@ function SmallPantheonCard(props) {
       return <Link to={`/pantheons/${item.pantheon_id}`} style={{ backgroundImage: `url(${item.image_url ? image_thumb(item.image_url) : null})` }} className='blockLink pantheonStyleLink'>
             <Row style={{ width: '100%', height: '100%', marginLeft: '0' }}>
                   <Col lg={3} xs={12} style={{padding:'0'}}>
-                        <h3 className="pantheonStyleName">{item.pantheon_name}</h3>
+                        <h3 className="pantheonStyleName">{item.pantheon_name}<br />
+                        <span style={{fontSize: '14px'}}>{item.start_year < 0 ? item.start_year * -1 + "BC" : item.start_year + "AD"} -
+                        {item.end_year < 0 ? item.end_year * -1 + "BC" : (item.end_year === 2100 ? "Present" : item.end_year + "AD")}<br />
+                        </span></h3>
                   </Col>
                   <Col lg={9} xs={12} style={{padding:'0'}}>
                         <p className="pantheonStyleText">
-                              {item.start_year < 0 ? item.start_year * -1 + "BC" : item.start_year + "AD"} -
-                        {item.end_year < 0 ? item.end_year * -1 + "BC" : (item.end_year === 2100 ? "Present" : item.end_year + "AD")}<br />
-                              {item.pantheon_description}
+                                    {item.pantheon_description}
                         </p>
                   </Col>
             </Row>
